d83af39f033503f61393ef3f1d97e7403c207157
Yashi <sharmayashi270@gmail.com>
2026-06-03 23:30:51 +0530

    db connection error solve

--- CapitaCare-backend/config/db.js
+++ CapitaCare-backend/config/db.js
@@ -1,8 +1,8 @@
-const mongoose = require("mongoose")
+const mongoose = require("mongoose");
 
 const connectDB = async () => {
     try{
-        await mongoose.connect(process.env.MONGO_URI, {})
+        await mongoose.connect(process.env.MONGO_URI);
         console.log("mongo db connected !");
         
     }
